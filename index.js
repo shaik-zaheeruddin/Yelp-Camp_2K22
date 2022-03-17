17b364f9b44b5482b78c62160a9c1c61cce36dfe
== <zaheerpersonal5@gmail.com>
2022-03-18 01:22:39 +0530

    git hub

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 
-require('dotenv').config()
+// require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
--- public/javascript/map.js
+++ public/javascript/map.js
@@ -1,19 +1,43 @@
+// mapboxgl.accessToken = token;
+// const map = new mapboxgl.Map({
+//     container: 'map', // container ID
+//     style: 'mapbox://styles/mapbox/light-v10', // style URL
+//     center: loc.geometry.coordinates, // starting position [lng, lat]
+//     zoom: 9 // starting zoom
+// });
+// map.addControl(new mapboxgl.NavigationControl());
+// new mapboxgl.Marker()
+//     .setLngLat(loc.geometry.coordinates)
+//     .setPopup(
+//         new mapboxgl.Popup({ offset: 25 })
+//             .setHTML(
+//                 `<h3>${loc.title}</h3><p>${loc.location}</p>`
+//             )
+//     )
+//     .addTo(map)
+
+
+
+// console.log(loc.geometry.coordinates)
 mapboxgl.accessToken = token;
 const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/light-v10', // style URL
-    center: loc.geometry.coordinates, // starting position [lng, lat]
-    zoom: 9 // starting zoom
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v10', // stylesheet location
+    center: campground.geometry.coordinates, // starting position [lng, lat]
+    zoom: 10 // starting zoom
 });
 
+map.addControl(new mapboxgl.NavigationControl());
+
+
 new mapboxgl.Marker()
-    .setLngLat(loc.geometry.coordinates)
+    .setLngLat(campground.geometry.coordinates)
     .setPopup(
         new mapboxgl.Popup({ offset: 25 })
             .setHTML(
-                `<h3>${loc.title}</h3><p>${loc.location}</p>`
+                `<h3>${campground.title}</h3><p>${campground.location}</p>`
             )
     )
     .addTo(map)
 
-map.addControl(new mapboxgl.NavigationControl());
+console.log(campground.geometry.coordinates)
