b7c45d4a655f97be3e40cc7c3280bfb9ac6aa13f
== <zaheerpersonal5@gmail.com>
2022-03-18 00:23:35 +0530

    add start script

--- index.js
+++ index.js
@@ -146,6 +146,7 @@ app.use((err, req, res, next) => {
     }
     res.status(status).render('Error.ejs', { err })
 })
-app.listen(3000, () => {
-    console.log('Serving on port 3000')
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log('Serving on port ' + port)
 })
